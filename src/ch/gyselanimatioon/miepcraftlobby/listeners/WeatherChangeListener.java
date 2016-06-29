2f6c9a23147a3cb7db0211991071df386f6b1c1c
Fabian Gysel <fabiangysel15@gmail.com>
2016-06-29 18:49:53 +0200

    wetter event wird immer gecancelt

--- src/ch/gyselanimatioon/miepcraftlobby/listeners/WeatherChangeListener.java
+++ src/ch/gyselanimatioon/miepcraftlobby/listeners/WeatherChangeListener.java
@@ -1,6 +1,5 @@
 package ch.gyselanimatioon.miepcraftlobby.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
@@ -9,7 +8,6 @@ public class WeatherChangeListener implements Listener {
 	
 	@EventHandler
 	public void WeatherChanged(WeatherChangeEvent event) {
-		Bukkit.getWorld(event.getWorld().getName()).setStorm(false);
-		Bukkit.getWorld(event.getWorld().getName()).setThundering(false);
+		event.setCancelled(true);
 	}
 }
